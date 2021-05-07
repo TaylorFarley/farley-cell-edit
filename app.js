b836e6b32306e0023a39177061e946ad01401a52
Taylor <twfarley88@gmail.com>
2021-05-06 21:52:13 -0400

    fix

--- app.js
+++ app.js
@@ -5,26 +5,37 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 
+
+
 var app = express();
 
-  
+
+
+
+
+//
+
 var cors = require('cors')
 
 app.use(cors()) // Use this after the variable declaration
 
+
 app.use(logger('dev'));
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({ extended: false, limit: '50mb' }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
+
+
+
+//DB SETUP
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
+.then(()=>{console.log('CONNECTED!!!!!!!!!!!!:)')});
+var db = mongoose.connection;
+//DB SETUP
 
 if(process.env.PORT){
   app.use(express.static(path.join(__dirname, "client/build")));
@@ -32,9 +43,7 @@ if(process.env.PORT){
     res.sendFile(path.join(__dirname + "/client/build/index.html"));
   });
   
-
-
-
+  
   app.use((req, res, next) => {
     next(createError(404));
   });
@@ -45,5 +54,7 @@ if(process.env.PORT){
     res.status(err.statusCode).send(err.message);
   });
   }
+  
+
 
 module.exports = app;
