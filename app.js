a47d36be9c48262abcec2c3fd5f6c407a2efe33f
Taylor <twfarley88@gmail.com>
2021-05-06 22:07:13 -0400

    cell up

--- app.js
+++ app.js
@@ -4,11 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-
-
-
 var app = express();
-
 
 
 
--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Home from './Home'
 function App() {
   return (
- <>HELLO</>
+ <Home />
   );
 }
 
